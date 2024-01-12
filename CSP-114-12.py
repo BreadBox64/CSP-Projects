277f52b477fa9d95d3f0f1db0537ae1c19a7cf16
BreadBox64 <eli137buy@gmail.com>
2024-01-12 17:36:17 -0600

    nuh uh

--- CSP-114-12.py
+++ CSP-114-12.py
@@ -5,8 +5,8 @@ color1 = "orange"
 color2 = "purple"
 
 wn = trtl.Screen()
-width = 400
-height = 300
+width = 200
+height = 200
 
 painter = trtl.Turtle()
 painter.speed(0)
@@ -22,12 +22,13 @@ while (answer == "y"):
 	seg = int(360/angle)
 
 	while painter.ycor() < height:
-		if painter.pencolor() == color2:
-			painter.fillcolor(color1)
-			painter.color(color1)
-		else:
-			painter.fillcolor(color2)
-			painter.color(color2)
+		if space % 100 == 0:
+			if painter.pencolor() == color2:
+				painter.fillcolor(color1)
+				painter.color(color1)
+			else:
+				painter.fillcolor(color2)
+				painter.color(color2)
 		painter.right(angle)
 		painter.forward(2 * space + 10) # experiment
 		painter.begin_fill()
