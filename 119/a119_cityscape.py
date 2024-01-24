b8855ff42f2203f5009fe1869f490de211ad47ff
BreadBox64 <eli137buy@gmail.com>
2024-01-24 03:18:44 -0600

    119 doie thingy

--- 119/a119_cityscape.py
+++ 119/a119_cityscape.py
@@ -2,23 +2,38 @@ import turtle
 import random
 import math
 
-test = turtle.Turtle()
-
-for i in range(3):
-	test.fd(100)
-	test.rt(120)
-
 screen = turtle.Screen()
 screen.colormode(255)
 
-color = [200, 100, 100]
-indexes = [1, 2, 0]
-while True:
-	for i in range(3):
-		for j in range(100):
-			color[i] -= 1
-			color[indexes[i]] += 1
-			screen.bgcolor(color)
+# Building Aid Functions
+oldBuildingShapeVars = {
+	"width": 30,
+	"height": 10,
+	"wallColor": "#AAAAAA",
+	"outlineColor": "#666666",
+	"windowColor": False
+}
+def redefineBuildingShape(usePreviousValues = False, width = False, height = False, wallColor = False, outlineColor = False, windowColor = False):
+	width = oldBuildingShapeVars.width if (usePreviousValues and not width) else (width if width else 30)
+	height = oldBuildingShapeVars.height if (usePreviousValues and not height) else (height if height else 10)
+	wallColor = oldBuildingShapeVars.wallColor if (usePreviousValues and not wallColor) else (wallColor if wallColor else "#AAAAAA")
+	outlineColor = oldBuildingShapeVars.outlineColor if (usePreviousValues and not outlineColor) else (outlineColor if outlineColor else "#666666")
+	windowColor = oldBuildingShapeVars.windowColor if (usePreviousValues and not windowColor) else windowColor
 
+	buildingFloor = turtle.Shape("compound")
+	halfWidth = width/2
+	buildingFloor.addcomponent(((0, 0), (width, halfWidth), (width, halfWidth + height), (0, height), (-width, halfWidth + height), (-width, halfWidth)), wallColor, outlineColor)
+	buildingFloor.addcomponent(((0, 0), (0, height)), outlineColor, outlineColor)
+	screen.register_shape("buildingFloor", buildingFloor)
+
+# Init Building Turtle
+redefineBuildingShape(False, 30, 10, "#AAAAAA", "#666666")
+buildingTurtle = turtle.Turtle()
+buildingTurtle.shape("buildingFloor")
+buildingTurtle.pu()
+buildingTurtle.seth(90)
+buildingTurtle.stamp()
+buildingTurtle.fd(10)
+buildingTurtle.stamp()
 
 screen.mainloop()
