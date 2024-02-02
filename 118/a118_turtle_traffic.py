d2bd3fa3afdac233e959802a81c783057d3fc6dc
QqWwertyuiop <s51626@parkhill.k12.mo.us>
2024-02-02 10:58:19 -0600

    qwertyuiop

--- 118/a118_turtle_traffic.py
+++ 118/a118_turtle_traffic.py
@@ -12,9 +12,12 @@ vert_turtles = []
 turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
 horiz_colors = ["red", "blue", "green", "orange", "purple", "gold"]
 vert_colors = ["darkred", "darkblue", "lime", "salmon", "indigo", "brown"]
+oldHorizColors = horiz_colors.copy()
+oldVertColors = vert_colors.copy()
 
 wn = trtl.Screen()
-wn.setworldcoordinates(-400, -100, 100, 400)
+wn.setworldcoordinates(-400, -1400, 1400, 400)
+wn.delay(1)
 
 tloc = 50
 for s in turtle_shapes:
@@ -38,29 +41,41 @@ for s in turtle_shapes:
 
   tloc += 50
 
-oldColors = []
-
-def collision(t0, s0, t1, s1):
+def collision(t0, s0, i0, t1, s1, i1):
   a = (t0.xcor() + s0 + 20) < t1.xcor()
   b = (t0.xcor() - 20) > t1.xcor()
   c = ((t1.ycor() - s1) - 20) > t0.ycor()
   d = (t1.ycor() + 20) < t0.ycor()
-  print(f"[{str(a)}, {str(b)}, {str(c)}, {str(d)}]")
-  if (a or b) and (c or d):
+  #print(f"[{str(a)}, {str(b)}, {str(c)}, {str(d)}]")
+  print(f"{i0}, {i1}")
+  if (a or b) or (c or d): # Not colliding on x or not colliding on y
+    t0.color(oldHorizColors[i0])
     t0.fd(s0)
-    oldColors.index(t1)
+    t1.color(oldVertColors[i1])
     t1.fd(s1)
   else:
-    t0.fd(s0)
-    t1.fd(s1)
-    oldColors[t1.color()[1]] = t1
-    t1.color("grey")
-    t1.bk(s1)
+    if (a or b): # Not colliding on x - therefore, Colliding on y
+      t1.fd(s1)
+      color = t1.color()[1]
+      if color != "grey":
+        oldVertColors[i0] = color
+      t0.color("grey")
+    else: # Colliding on x
+      t0.fd(s0)
+      color = t1.color()[1]
+      if color != "grey":
+        oldHorizColors[i1] = color
+      t1.color("grey")
 
 for i in range(50):
+  h = 0
   for ht in horiz_turtles:
+    v = 0
     for vt in vert_turtles:
-      collision(ht, 2, vt, 2)    
+      dist = 1
+      collision(ht, dist, h, vt, dist, v)
+      v += 1
+    h += 1
 
 for i in [horiz_turtles, vert_turtles]:
   for t in i:
