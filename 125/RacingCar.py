16cd70aec0882bfd0f423aaea5e678aebaab1459
BreadBox64 <eli137buy@gmail.com>
2024-02-10 21:08:50 -0600

    a125 - Initial Commit + Created RacingCar class + Key listeners and screen update in main file

---
+++ 125/RacingCar.py
@@ -0,0 +1,76 @@
+import math
+from turtle import *
+from numpy import cross
+
+def vRotate(vec:Vec2D, angle:float) -> Vec2D:
+	rad = math.radians(angle)
+	cos = math.cos(rad)
+	sin = math.sin(rad)
+	return Vec2D(vec[0]*cos + vec[1]*cos, vec[1]*sin + vec[0]*sin)
+
+def vHeading(vec:Vec2D) -> float:
+	rad = math.atan2(vec[1], vec[0])
+	return math.degrees(rad)
+
+def vMag(vec:Vec2D) -> float:
+	return math.sqrt(vec[0]**2 + vec[1]**2)
+
+def vNorm(vec:Vec2D, len:float) -> Vec2D:
+	return vec * (len/vMag(vec))
+
+class RacingCar:
+	frictionCoeff:float
+	pos:Vec2D
+	vel:Vec2D
+	accInertia:int
+	rotInertia:int
+	heading:float
+	inputs:dict
+	
+	def __init__(self, _pos:Vec2D=Vec2D(0, 0), _vel:Vec2D=Vec2D(0, 0), _heading:float=0.0, _frictionCoeff:float=0.2) -> None:
+		self.pos = _pos
+		self.vel = _vel
+		self.heading = _heading
+		self.frictionCoeff = _frictionCoeff
+		self.accInertia = 0.0
+		self.rotInertia = 0.0
+		self.maxSpeed = 15
+		self.inputs = { # lowercase represents the input inertia, capital the condition of the key
+			'W': False,
+			'A': False,
+			'S': False,
+			'D': False
+		}
+
+	def _step(self) -> None:
+		inputs = self.inputs
+		# Input inertia step
+		fd = int(inputs['W'])-int(inputs['S']) * 0.01
+		if fd != 0:
+			self.accInertia += fd if fd + self.accInertia <= 2 and fd + self.accInertia >= -1 else 0 
+		else:
+			self.accInertia = 0
+		rot = (int(inputs['A'])-int(inputs['D'])) * 0.5
+		if rot != 0:
+			self.rotInertia += rot if rot + self.rotInertia <= 5 and rot + self.rotInertia >= -5 else 0 
+		else:
+			self.rotInertia = 0
+
+		# Actual physics step
+		acc = vRotate((self.accInertia, 0), self.heading)
+		#print(str(acc))
+		acc -= self.vel * (self.frictionCoeff/(vMag(self.vel) + 1))
+		acc -= self.vel * 0.4 * math.sin(math.radians(abs(self.heading - vHeading(self.vel))))
+		self.heading += self.rotInertia
+		self.vel += acc
+		if vMag(self.vel) > self.maxSpeed:
+			self.vel = vNorm(self.vel, self.maxSpeed)
+		self.pos += self.vel
+		
+	def draw(self, trtl:Turtle) -> None:
+		self._step()
+		trtl.goto(self.pos)
+		trtl.seth(self.heading)
+
+	def inputChanged(self, key:str, val:bool) -> None:
+		self.inputs[key] = val
---
+++ 125/a125_main.py
@@ -0,0 +1,46 @@
+from turtle import *
+from RacingCar import *
+#import TrackManager
+
+screen = Screen()
+screen.delay(0)
+trtl = Turtle()
+trtl.speed(0)
+trtl.pu()
+rc = RacingCar()
+inputs = {
+	'W': False,
+	'A': False,
+	'S': False,
+	'D': False
+}
+
+def genKF(key:str, val:bool):
+	def f():
+		keyVal = inputs[key]
+		if keyVal == val:
+			#print("r")
+			pass
+		else:
+			#print(f"KF - {key} - {val}")
+			globals()['inputs'][key] = val
+			rc.inputChanged(key, val)
+	return f
+kpFunctions = []
+krFunctions = []
+keys = ['w', 'a', 's', 'd']
+for key in keys:
+	kpFunctions.append(genKF(key.capitalize(), True))
+	krFunctions.append(genKF(key.capitalize(), False))
+for i,f in enumerate(kpFunctions):
+	screen.onkeypress(f, keys[i])
+for i,f in enumerate(krFunctions):
+	screen.onkeyrelease(f, keys[i])
+
+def screenUpdate():
+	rc.draw(trtl)
+	screen.ontimer(screenUpdate, 20)
+
+screen.listen()
+screenUpdate()
+screen.mainloop()
