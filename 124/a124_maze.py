d5b067a970d4ad3c6471c11adeb033ec0ca54895
BreadBox64 <eli137buy@gmail.com>
2024-02-14 20:37:28 -0600

    124 - Doors

--- 124/a124_maze.py
+++ 124/a124_maze.py
@@ -7,7 +7,14 @@ trtl = turtle.Turtle()
 trtl.speed(0)
 
 for i in range(10, 400, 10):
-	trtl.fd(i)
+	if i > 30:
+		trtl.fd(i-20)
+		trtl.pu()
+		trtl.fd(10)
+		trtl.pd()
+		trtl.fd(10)
+	else:
+		trtl.fd(i)
 	trtl.rt(90)
 
 screen.mainloop()
