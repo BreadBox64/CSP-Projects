1b2d1fd463f91f943e7b7ec12bf1ddeed692704f
BreadBox64 <eli137buy@gmail.com>
2024-02-14 21:01:38 -0600

    124 - Random Maze

--- 124/a124_maze.py
+++ 124/a124_maze.py
@@ -1,4 +1,5 @@
 import turtle
+from random import randint
 
 screen = turtle.Screen()
 screen.setup(600, 600)
@@ -6,19 +7,29 @@ screen.screensize(200, 200)
 trtl = turtle.Turtle()
 trtl.speed(0)
 
-width = 10
-for i in range(width, 400, width):
+width = 20
+for i in range(width, 400, int(width*0.5)):
+	trtl.pd()
 	if i > 3*width:
-		trtl.fd(i-(3*width))
-		trtl.rt(90)
-		trtl.fd(width*2)
-		trtl.bk(width*2)
-		trtl.lt(90)
-		trtl.fd(width)
+		doorPos = randint(0, i-width)
+		barrierPos = randint(width, i-width)
+		barrierPos = barrierPos if barrierPos <= doorPos or barrierPos >= (doorPos+width) else doorPos
+		# Door
+		trtl.fd(doorPos)
 		trtl.pu()
 		trtl.fd(width)
 		trtl.pd()
+		trtl.fd(i-(doorPos+width))
+		# Barrier
+		trtl.pu()
+		trtl.bk(barrierPos)
+		trtl.rt(90)
+		trtl.pd()
 		trtl.fd(width)
+		trtl.pu()
+		trtl.bk(width)
+		trtl.lt(90)
+		trtl.fd(barrierPos)
 	else:
 		trtl.fd(i)
 	trtl.rt(90)
