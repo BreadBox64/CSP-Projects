074a944c7e22cce442d9422562b4e7cc0e3b8033
BreadBox64 <eli137buy@gmail.com>
2024-02-16 09:46:35 -0600

    124 - efficient

--- 124/a124_maze.py
+++ 124/a124_maze.py
@@ -3,14 +3,22 @@ import random
 
 screen = turtle.Screen()
 screen.setup(600, 600)
+screen.delay(0)
 screen.screensize(200, 200)
-trtl = turtle.Turtle()
-trtl.speed(0)
+drawTrtl = turtle.Turtle()
+drawTrtl.speed(0)
+mazeTrtl = turtle.Turtle()
+mazeTrtl.speed(0)
+mazeTrtl.pu()
+mazeTrtl.shape("turtle")
+timer = 0
+game = False
+x = 0
+y = 0 #trtl coords dont work
 
 width = 20
-lastDoors = [0, 0, 0, 0]
 
-def genBarrierPos(doorPos, lastDoor, len) -> int:
+def genBarrierPos(doorPos:int, lastDoor:int, len:int) -> int:
 	posList:list = list(range(width, len-width))
 	for i in range(doorPos, doorPos+width):
 		try:
@@ -24,31 +32,44 @@ def genBarrierPos(doorPos, lastDoor, len) -> int:
 			pass
 	return random.choice(posList)
 
-for i in range(width, 400, int(width*0.5)):
-	trtl.pd()
-	if i > 3*width:
-		lastDoor = lastDoors.pop(0)
-		doorPos = random.randint(0, i-width)
-		barrierPos = genBarrierPos(doorPos, lastDoor, i)
-		lastDoors.append(doorPos+width)
-		# Door
-		trtl.fd(doorPos)
-		trtl.pu()
-		trtl.fd(width)
-		trtl.pd()
-		trtl.fd(i-(doorPos+width))
-		# Barrier
-		trtl.pu()
-		trtl.bk(i-barrierPos)
-		trtl.rt(90)
-		trtl.pd()
-		trtl.fd(width)
-		trtl.pu()
-		trtl.bk(width)
-		trtl.lt(90)
-		trtl.fd(i-barrierPos)
-	else:
-		trtl.fd(i)
-	trtl.rt(90)
+def setup():
+	mazeTrtl.goto(10, -15)
+	globals()['x'] = 10
+	globals()['y'] = -15
+	drawTrtl.st()
+	drawTrtl.goto(0, 0)
+	drawTrtl.seth(0)
+	drawTrtl.clear()
+	lastDoors = [0, 0, 0, 0]
+	for i in range(width, 440, int(width*0.5)):
+		drawTrtl.pd()
+		if i > 3*width and i < 400:
+			lastDoor = lastDoors.pop(0)
+			doorPos = random.randint(0, i-width)
+			barrierPos = genBarrierPos(doorPos, lastDoor, i)
+			lastDoors.append(doorPos+width)
+			# Door
+			drawTrtl.fd(doorPos)
+			drawTrtl.pu()
+			drawTrtl.fd(width)
+			drawTrtl.pd()
+			drawTrtl.fd(i-(doorPos+width))
+			# Barrier
+			drawTrtl.pu()
+			drawTrtl.bk(i-barrierPos)
+			drawTrtl.rt(90)
+			drawTrtl.pd()
+			drawTrtl.fd(width)
+			drawTrtl.pu()
+			drawTrtl.bk(width)
+			drawTrtl.lt(90)
+			drawTrtl.fd(i-barrierPos)
+		else:
+			drawTrtl.fd(i)
+		drawTrtl.rt(90)
+	drawTrtl.fd(420)
+	drawTrtl.ht()
 
+setup()
+screen.listen()
 screen.mainloop()
